49a1e49904d9912b41823970596d67e5c2f95788
Temptica <nicovsgaming@gmail.com>
2025-07-03 18:39:23 +0200

    V0.0.7

--- GodotObjectExtensions.cs
+++ GodotObjectExtensions.cs
@@ -47,6 +47,54 @@ public static class GodotObjectExtensions
 
 		return taskCompletionSource.Task;
 	}
+	
+	public static async Task<TArg0?> ToSignalAsync<TArg0>(this GodotObject godotObject, StringName signal)
+	{
+		var results = await ToSignalAsync(godotObject, signal);
+
+		if (results.Length < 1)
+		{
+			throw new InvalidOperationException($"The signal has {results.Length} parameters but expected at least 1.");
+		}
+
+		var arg0 = CastOrThrow<TArg0>(results, 0);
+		return arg0;
+	}
+
+	public static async Task<(TArg0?, TArg1?)> ToSignalAsync<TArg0, TArg1>(
+		this GodotObject godotObject,
+		StringName signal
+	)
+	{
+		var results = await ToSignalAsync(godotObject, signal);
+
+		if (results.Length < 2)
+		{
+			throw new InvalidOperationException($"The signal has {results.Length} parameters but expected at least 2.");
+		}
+
+		var arg0 = CastOrThrow<TArg0>(results, 0);
+		var arg1 = CastOrThrow<TArg1>(results, 1);
+		return (arg0, arg1);
+	}
+
+	public static async Task<(TArg0?, TArg1?, TArg2?)> ToSignalAsync<TArg0, TArg1, TArg2>(
+		this GodotObject godotObject,
+		StringName signal
+	)
+	{
+		var results = await ToSignalAsync(godotObject, signal);
+
+		if (results.Length < 2)
+		{
+			throw new InvalidOperationException($"The signal has {results.Length} parameters but expected at least 3.");
+		}
+
+		var arg0 = CastOrThrow<TArg0>(results, 0);
+		var arg1 = CastOrThrow<TArg1>(results, 1);
+		var arg2 = CastOrThrow<TArg2>(results, 2);
+		return (arg0, arg1, arg2);
+	}
 
 	private static bool TryCast<TValue>(Variant variant, [NotNullWhen(true)] out TValue? value)
 	{
@@ -160,73 +208,41 @@ public static class GodotObjectExtensions
 
 				break;
 			case Variant.Type.Vector2:
-				break;
 			case Variant.Type.Vector2I:
-				break;
 			case Variant.Type.Rect2:
-				break;
 			case Variant.Type.Rect2I:
-				break;
 			case Variant.Type.Vector3:
-				break;
 			case Variant.Type.Vector3I:
-				break;
 			case Variant.Type.Transform2D:
-				break;
 			case Variant.Type.Vector4:
-				break;
 			case Variant.Type.Vector4I:
-				break;
 			case Variant.Type.Plane:
-				break;
 			case Variant.Type.Quaternion:
-				break;
 			case Variant.Type.Aabb:
-				break;
 			case Variant.Type.Basis:
-				break;
 			case Variant.Type.Transform3D:
-				break;
 			case Variant.Type.Projection:
-				break;
 			case Variant.Type.Color:
-				break;
 			case Variant.Type.StringName:
-				break;
 			case Variant.Type.NodePath:
-				break;
 			case Variant.Type.Rid:
-				break;
 			case Variant.Type.Object:
-				break;
 			case Variant.Type.Callable:
-				break;
 			case Variant.Type.Signal:
-				break;
 			case Variant.Type.Dictionary:
-				break;
 			case Variant.Type.Array:
-				break;
 			case Variant.Type.PackedByteArray:
-				break;
 			case Variant.Type.PackedInt32Array:
-				break;
 			case Variant.Type.PackedInt64Array:
-				break;
 			case Variant.Type.PackedFloat32Array:
-				break;
 			case Variant.Type.PackedFloat64Array:
-				break;
 			case Variant.Type.PackedStringArray:
-				break;
 			case Variant.Type.PackedVector2Array:
-				break;
 			case Variant.Type.PackedVector3Array:
-				break;
 			case Variant.Type.PackedColorArray:
-				break;
 			case Variant.Type.Max:
 				break;
+			case Variant.Type.PackedVector4Array:
 			default:
 				throw new NotImplementedException();
 		}
@@ -250,53 +266,5 @@ public static class GodotObjectExtensions
 		throw new InvalidOperationException(
 			$"Expected index {index} to be {expectedTypeName} but it was {actualTypeName}"
 		);
-	}
-
-	public static async Task<TArg0?> ToSignalAsync<TArg0>(this GodotObject godotObject, StringName signal)
-	{
-		var results = await ToSignalAsync(godotObject, signal);
-
-		if (results.Length < 1)
-		{
-			throw new InvalidOperationException($"The signal has {results.Length} parameters but expected at least 1.");
-		}
-
-		var arg0 = CastOrThrow<TArg0>(results, 0);
-		return arg0;
-	}
-
-	public static async Task<(TArg0?, TArg1?)> ToSignalAsync<TArg0, TArg1>(
-		this GodotObject godotObject,
-		StringName signal
-	)
-	{
-		var results = await ToSignalAsync(godotObject, signal);
-
-		if (results.Length < 2)
-		{
-			throw new InvalidOperationException($"The signal has {results.Length} parameters but expected at least 2.");
-		}
-
-		var arg0 = CastOrThrow<TArg0>(results, 0);
-		var arg1 = CastOrThrow<TArg1>(results, 1);
-		return (arg0, arg1);
-	}
-
-	public static async Task<(TArg0?, TArg1?, TArg2?)> ToSignalAsync<TArg0, TArg1, TArg2>(
-		this GodotObject godotObject,
-		StringName signal
-	)
-	{
-		var results = await ToSignalAsync(godotObject, signal);
-
-		if (results.Length < 2)
-		{
-			throw new InvalidOperationException($"The signal has {results.Length} parameters but expected at least 3.");
-		}
-
-		var arg0 = CastOrThrow<TArg0>(results, 0);
-		var arg1 = CastOrThrow<TArg1>(results, 1);
-		var arg2 = CastOrThrow<TArg2>(results, 2);
-		return (arg0, arg1, arg2);
 	}
 }
