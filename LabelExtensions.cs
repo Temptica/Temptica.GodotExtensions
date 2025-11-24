0d3c57f41464cc812586783206af1797a59fa671
Temptica <nicovsgaming@gmail.com>
2025-11-24 21:35:10 +0100

    Re-add launcher. Fix ui bugs and perfo issues

--- LabelExtensions.cs
+++ LabelExtensions.cs
@@ -5,12 +5,39 @@ namespace Temptica.GodotExtensions;
 
 public static class LabelExtensions
 {
-    public static void SetTranslatedText(this Label label, string text)
+    public static void SetTranslatedText(this Label label, string text, params object?[] args)
     {
         label.Text = TranslationServer.Translate(text);
         if (label.Text == text)
         {
             label.Text = $"NO TRANSLATION: {text}";
+            return;
         }
+
+        if (args.Length > 0) label.Text = string.Format(label.Text, args);
+    }
+    
+    public static void SetTranslatedText(this Label3D label, string text, params object?[] args)
+    {
+        label.Text = TranslationServer.Translate(text);
+        if (label.Text == text)
+        {
+            label.Text = $"NO TRANSLATION: {text}";
+            return;
+        }
+
+        if (args.Length > 0) label.Text = string.Format(label.Text, args);
+    }
+
+    public static void SetTranslatedText(this RichTextLabel label, string text, params object?[] args)
+    {
+        label.Text = TranslationServer.Translate(text);
+        if (label.Text == text)
+        {
+            label.Text = $"NO TRANSLATION: {text}";
+            return;
+        }
+
+        if (args.Length > 0) label.Text = string.Format(label.Text, args);
     }
 }
