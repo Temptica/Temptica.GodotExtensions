35e346106fba9ee9725d6c44ec628f400af74b2b
Temptica <nic.vanruy@gmail.com>
2026-05-06 20:02:05 +0200

    Implement first 5 steps of tutorial

--- LabelExtensions.cs
+++ LabelExtensions.cs
@@ -8,36 +8,18 @@ public static class LabelExtensions
     public static void SetTranslatedText(this Label label, string text, params object?[] args)
     {
         label.Text = TranslationServer.Translate(text);
-        if (label.Text == text)
-        {
-            label.Text = $"NO TRANSLATION: {text}";
-            return;
-        }
-
         if (args.Length > 0) label.Text = string.Format(label.Text, args);
     }
-    
+
     public static void SetTranslatedText(this Label3D label, string text, params object?[] args)
     {
         label.Text = TranslationServer.Translate(text);
-        if (label.Text == text)
-        {
-            label.Text = $"NO TRANSLATION: {text}";
-            return;
-        }
-
         if (args.Length > 0) label.Text = string.Format(label.Text, args);
     }
 
     public static void SetTranslatedText(this RichTextLabel label, string text, params object?[] args)
     {
         label.Text = TranslationServer.Translate(text);
-        if (label.Text == text)
-        {
-            label.Text = $"NO TRANSLATION: {text}";
-            return;
-        }
-
         if (args.Length > 0) label.Text = string.Format(label.Text, args);
     }
 }
